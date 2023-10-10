ba2f5e5c09b48c8d8461db0df11d803c28c996f3
awdem <wdemontmollin@gmail.com>
2023-10-10 17:23:53 +0100

    refactor: adds new Parser class to Scraper, minor edits

--- scraper/Scraper.js
+++ scraper/Scraper.js
@@ -1,28 +1,27 @@
 const axios = require('axios');
-const parse = require('./parse');
+const Parser = require('./Parser');
 const { load } =  require('cheerio')
-
-
 
 class Scraper {
 
 	constructor() {
+		this.parser = new Parser;
 		this.day = 1
 		this.month = 1
 	};
 
-	sleep(ms) {
+	delayRate(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
 	
 	async scrape(url) {
 		let resp = await axios.get(url);
-		await this.sleep(1200);
+		await this.delayRate(1200);
 	
-		let resp_html = resp.data;
-		const $ = load(resp_html);
+		let respHTML = resp.data;
+		const $ = load(respHTML);
 	
-		parse($);
+		this.parser.parse($);
 	
 		let daysInMonth = $(`#mw-content-text > div.mw-parser-output > div.navbox > table > tbody > tr:nth-child(${this.month + 2}) > td > div > ul`).children('li').length
 		this.day += 1;
@@ -36,18 +35,16 @@ class Scraper {
 		// console.log(this.month);
 	
 		if ( this.month < 13 ) {
-			let next_href = $(`#mw-content-text > div.mw-parser-output > div.navbox > table > tbody > tr:nth-child(${this.month + 2}) > td > div > ul > li:nth-child(${this.day}) > a`).attr('href');
-			let next_url = 'https://en.wikipedia.org' + next_href
+			let nextHref = $(`#mw-content-text > div.mw-parser-output > div.navbox > table > tbody > tr:nth-child(${this.month + 2}) > td > div > ul > li:nth-child(${this.day}) > a`).attr('href');
+			let nextUrl = 'https://en.wikipedia.org' + nextHref
 			
-			// console.log(next_url)
-			this.scrape(next_url)
+			// console.log(nextUrl)
+			this.scrape(nextUrl)
 		} else {
 			console.log('scrape complete')
 			return
 		}
 	}
 }
-
-
 
 module.exports = Scraper;
