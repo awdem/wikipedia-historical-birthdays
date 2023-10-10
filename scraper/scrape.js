77e68cc55857e53080911d4c178edb8abd19ad81
awdem <wdemontmollin@gmail.com>
2023-10-10 11:05:57 +0100

    feat: finishes scraper iteration logic

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -12,7 +12,7 @@ function sleep(ms) {
 
 scrape = async (url) => {
 	let resp = await axios.get(url);
-	await sleep(1000);
+	await sleep(1200);
 
 	let resp_html = resp.data;
 	const $ = load(resp_html);
@@ -22,19 +22,19 @@ scrape = async (url) => {
 	let daysInMonth = $(`#mw-content-text > div.mw-parser-output > div.navbox > table > tbody > tr:nth-child(${month + 2}) > td > div > ul`).children('li').length
 	day += 1;
 
-	if (day >= daysInMonth) {
+	if (day > daysInMonth) {
 		month += 1;
 		day = 1
 	}
 
-	console.log(day);
-	console.log(month);
+	// console.log(day);
+	// console.log(month);
 
 	if ( month < 13 ) {
 		let next_href = $(`#mw-content-text > div.mw-parser-output > div.navbox > table > tbody > tr:nth-child(${month + 2}) > td > div > ul > li:nth-child(${day}) > a`).attr('href');
 		let next_url = 'https://en.wikipedia.org' + next_href
 		
-		console.log(next_url)
+		// console.log(next_url)
 		scrape(next_url)
 	} else {
 		console.log('scrape complete')
@@ -42,7 +42,5 @@ scrape = async (url) => {
 	}
 
 }
-
-scrape('https://en.wikipedia.org/wiki/January_1')
 
 module.exports = scrape;
