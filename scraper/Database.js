6edd8e4270fc49411396e765a0719ad8b7767326
awdem <wdemontmollin@gmail.com>
2023-10-13 14:31:08 +0100

    chore: sets up DB connection using .env file for user and password (installs dotenv)

--- scraper/Database.js
+++ scraper/Database.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const { Pool } = require("pg");
-const { connect_timeout } = require("pg/lib/defaults");
 
 class Database {
 	constructor() {
 		this.db = new Pool({
-			user: "temp",
+			user: '',
 			host: "localhost",
-			database: "temp",
-			password: "temp",
+			database: "wiki_birthdays_test",
+			password: '',
 			port: 5432,
 		})
 	}
@@ -20,6 +20,10 @@ class Database {
 		} catch (err) {
 			console.log('Error connecting to database', err);
 		}
+	}
+
+	async createRecords(data, res) {
+		// TODO
 	}
 }
 
