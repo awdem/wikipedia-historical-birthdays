e4c9d9b6c4e02c3065a2209789fa3703b296af35
awdem <wdemontmollin@gmail.com>
2023-10-26 15:15:44 +0100

    tests: adds more outliers from first successful scrape to test html data and updates existing tests

--- scraper/Parser.test.js
+++ scraper/Parser.test.js
@@ -15,17 +15,17 @@ describe('Parser', () => {
 
 	it('returns an array of record objects with the names of the bday havers', () => {
 		expect(parser.parse($)[0].name).toEqual('Ali al-Ridha')
-		expect(parser.parse($)[5].name).toEqual('Hans von Dohnányi')
+		expect(parser.parse($)[7].name).toEqual('Hans von Dohnányi')
 	})
 
 	it('returns an array of record objects with the wiki url for each bday haver', () => {
 		expect(parser.parse($)[0].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Ali_al-Ridha')
-		expect(parser.parse($)[5].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Hans_von_Dohn%C3%A1nyi')
+		expect(parser.parse($)[7].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Hans_von_Dohn%C3%A1nyi')
 	})
 
 	it('returns an array of record objects with the bday date for each bday haver', () => {
 		expect(parser.parse($)[0].bday).toEqual('0766-01-01')
-		expect(parser.parse($)[5].bday).toEqual('1902-01-01')
+		expect(parser.parse($)[7].bday).toEqual('1902-01-01')
 	})
 
 	it('correctly checks if a bday date is CE or BCE', () => {
