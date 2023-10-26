9881217a6c166882c546956f5a1874a060209493
awdem <wdemontmollin@gmail.com>
2023-10-26 16:07:02 +0100

    tests: adds test html and test for names that start with a digit, currently passing

--- scraper/Parser.test.js
+++ scraper/Parser.test.js
@@ -15,17 +15,17 @@ describe('Parser', () => {
 
 	it('returns an array of record objects with the names of the bday havers', () => {
 		expect(parser.parse($)[0].name).toEqual('Ali al-Ridha')
-		expect(parser.parse($)[7].name).toEqual('Hans von Dohnányi')
+		expect(parser.parse($)[8].name).toEqual('Hans von Dohnányi')
 	})
 
 	it('returns an array of record objects with the wiki url for each bday haver', () => {
 		expect(parser.parse($)[0].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Ali_al-Ridha')
-		expect(parser.parse($)[7].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Hans_von_Dohn%C3%A1nyi')
+		expect(parser.parse($)[8].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Hans_von_Dohn%C3%A1nyi')
 	})
 
 	it('returns an array of record objects with the bday date for each bday haver', () => {
 		expect(parser.parse($)[0].bday).toEqual('0766-01-01')
-		expect(parser.parse($)[7].bday).toEqual('1902-01-01')
+		expect(parser.parse($)[8].bday).toEqual('1902-01-01')
 	})
 
 	it('correctly checks if a bday date is CE or BCE', () => {
@@ -54,11 +54,18 @@ describe('Parser', () => {
 		expect(parser.parse($)[5].BCE).toEqual(false)
 	})
 
-		xit('correctly handles year list elements that are followed by a citation', () => {
+	xit('correctly handles year list elements that are followed by a citation', () => {
 		expect(parser.parse($)[5].name).toEqual('Malcolm IV of Scotland')
 		expect(parser.parse($)[5].wikiUrl).toEqual('https://en.wikipedia.org/wiki/Malcolm_IV_of_Scotland')
 		expect(parser.parse($)[5].bday).toEqual('1141-01-01')
 		expect(parser.parse($)[5].BCE).toEqual(false)
+	})
+
+	it('correctly handles names that start with a digit', () => {
+		expect(parser.parse($)[6].name).toEqual('645AR')
+		expect(parser.parse($)[6].wikiUrl).toEqual('https://en.wikipedia.org/wiki/645AR')
+		expect(parser.parse($)[6].bday).toEqual('1997-01-01')
+		expect(parser.parse($)[6].BCE).toEqual(false)
 	})
 
 })
